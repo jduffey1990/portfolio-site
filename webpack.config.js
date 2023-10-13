3da8a36da52eecf4197c76f4266f75a647edb631
jduffey1990 <jduffey1990@gmail.com>
2023-10-13 14:27:24 -0600

    favicon

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html'
+      template: './index.html',
+      favicon: './favicon.png'
     }),
     new webpack.DefinePlugin({
       'process.env.PUBLIC_URL': JSON.stringify('/') // Adjusted to your provided path
