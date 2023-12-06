e5440385e5ac010fd64f0fd002217a8eb3deeca9
jduffey1990 <jduffey1990@gmail.com>
2023-12-06 16:57:32 -0700

    changes to new project

--- frontend/src/Pages/Projects/BusinessOptions.js
+++ frontend/src/Pages/Projects/BusinessOptions.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ProjectsCard from "./ProjectsCard";
+
 import TMB from "../../images/TMB.png"
+import cattle from "../../images/cattle.png"
 
 function BusinessOptions() {
 
@@ -13,7 +15,13 @@ function BusinessOptions() {
                 image={TMB}
                 shortDescription="Boutique Mortgage Broker in the Denver Area requested that I fabricate them a more modern and directly functional website than an 'out of the box' solution could manage." 
                 onClick={() => window.open("https://themortgagebureau.com", '_blank')}
-/>
+            />
+            <ProjectsCard 
+                title="Mann Ranch" 
+                image={cattle}
+                shortDescription="UNDER CONSTRUCTION-Nestled in the scenic Flint Hills of Kansas, stands as a beacon of excellence in the world of Red Angus cattle." 
+                onClick={() => window.open("http://mrflinthills.com.s3-website.us-east-2.amazonaws.com", '_blank')}
+            />
             </div>
         </>
 )}
